a97e9bc6917cfbc982ea3f231e94f313a2497d93
Kim Ji Eun <{>
2024-11-07 11:26:04 +0900

    오타수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import Dex from './Dex';
 import { useNavigate } from 'react-router-dom';
 import logo from '/src/assets/images/logo.png';
-
 function Home() {
-  const navigte = useNavigate();
+  const naviagte = useNavigate();
   return (
     <>
       <img src={logo} className="logo" />
       <div>
         <button
           onClick={() => {
-            navigte('/dex');
+            naviagte('/dex');
           }}
         >
           포켓몬 도감 시작하기
