ae4d9651684853466111520899114dd3ab864fbd
Kim Ji Eun <{>
2024-11-08 14:43:25 +0900

    components 폴더명 오타 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dex from './pages/Dex';
-import PokemonDetail from './compoents/PokemonDetail';
+import PokemonDetail from './components/PokemonDetail';
 
 function Router() {
   return (
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Dashboard from '../compoents/Dashboard';
-import PokemonList from '../compoents/PokemonList';
+import Dashboard from '../components/Dashboard';
+import PokemonList from '../components/PokemonList';
 
 function Dex() {
   const [pickedPokemonData, setPickedPokemonData] = useState([]);
