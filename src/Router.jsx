969abe3548c0609e2131301903030c7172e48550
Kim Ji Eun <{>
2024-11-13 00:03:27 +0900

    not found 페이지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dex from './pages/Dex';
 import PokemonDetail from './components/PokemonDetail';
+import NotFound from './components/NotFound';
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
           <Route path="/" element={<Home />} />
           <Route path="/dex" element={<Dex />} />
           <Route path="/pokemon/:id" element={<PokemonDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
